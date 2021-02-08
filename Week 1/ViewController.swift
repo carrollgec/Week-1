24cf52efaae5d123de1d2d56ba345ccc7aecc393
Grace <carrolhn@bc.edu>
2021-02-08 18:06:32 -0500

    Week 1 App with Constraints and Auto Resizing

--- Week 1/ViewController.swift
+++ Week 1/ViewController.swift
@@ -5,15 +5,22 @@
 //  Created by Grace Carroll on 2/1/21.
 //
 
-import UIKit
+import UIKit // given to use user interface package
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("😎 viewDidLoad has executed", Date())
     }
 
-
+    @IBAction func showMessagePressed(_ sender: UIButton) {
+        print("👉🏻 Show message button was pressed!", Date())
+        messageLabel.text="You are awesome!"
+    }
+    
 }
 
